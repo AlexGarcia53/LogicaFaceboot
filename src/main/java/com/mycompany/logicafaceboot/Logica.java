73e904a336f672f34f09371804dd1cfbe8e6ba2d
Alex García <alex.garcia228721@potros.itson.edu.mx>
2022-10-30 23:27:26 -0700

    primer version logica

---
+++ src/main/java/com/mycompany/logicafaceboot/Logica.java
@@ -0,0 +1,88 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.logicafaceboot;
+
+import datosPersistencia.FabricaDatosPersistencia;
+import datosPersistencia.IDatosPersistencia;
+import dominio.Comentario;
+import dominio.Hashtag;
+import dominio.Mensaje;
+import dominio.Publicacion;
+import dominio.Usuario;
+import interfaces.ILogica;
+import java.util.List;
+
+/**
+ *
+ * @author Admin
+ */
+public class Logica implements ILogica{
+    private IDatosPersistencia persistencia;
+    
+    public Logica(){
+        persistencia= FabricaDatosPersistencia.crearFachadaDatos();
+    }
+    
+    @Override
+    public void registrarUsuario(Usuario usuario) {
+        persistencia.registrarUsuario(usuario);
+    }
+
+    @Override
+    public void editarPerfilUsuario(Usuario usuario) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Usuario consultarUsuario(int idUsuario) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void registrarPublicacion(Publicacion publicacion) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void eliminarPublicacion(Publicacion publicacion) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Publicacion consultarPublicacionHashtag(Hashtag hashtag) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void registrarHashtag(Hashtag hashtag) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Hashtag consultarHashtag(int idHashtag) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void registrarComentario(Comentario comentario) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void eliminarComentario(Comentario comentario) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<Comentario> consultarComentarios(int idPublicacion) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void enviarNotificacion(Mensaje mensaje) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
---
+++ src/main/java/interfaces/ILogica.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package interfaces;
+
+import dominio.Comentario;
+import dominio.Hashtag;
+import dominio.Mensaje;
+import dominio.Publicacion;
+import dominio.Usuario;
+import java.util.List;
+
+/**
+ *
+ * @author Admin
+ */
+public interface ILogica {
+    public void registrarUsuario(Usuario usuario);
+    public void editarPerfilUsuario(Usuario usuario);
+    public Usuario consultarUsuario(int idUsuario);
+    public void registrarPublicacion(Publicacion publicacion);
+    public void eliminarPublicacion(Publicacion publicacion);
+    public Publicacion consultarPublicacionHashtag(Hashtag hashtag);
+    public void registrarHashtag(Hashtag hashtag);
+    public Hashtag consultarHashtag(int idHashtag);
+    public void registrarComentario(Comentario comentario);
+    public void eliminarComentario(Comentario comentario);
+    public List<Comentario> consultarComentarios(int idPublicacion);
+    public void enviarNotificacion(Mensaje mensaje);
+//    public void registrarTransaccion(Transaccion transaccion);
+}
